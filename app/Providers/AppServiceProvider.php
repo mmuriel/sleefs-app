d90b38d9eaed191076e68d5d1eb849fba4ef1329
Mauricio Muriel <mauricio.muriel@calitek.net>
2023-08-07 18:41:44 -0500

    Obligando a todos los links a ser https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+            //
+            //
+            if(config('app.env') === 'production' || config('app.env')==='local') {
+                 \URL::forceScheme('https');
+            }
     }
+
 }
